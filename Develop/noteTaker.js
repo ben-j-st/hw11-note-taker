2208a92341fcf3d396cc15cf603f95385c5590ca
Ben Stephens <ben_j_stephens@hotmail.com>
2020-07-27 12:37:26 +1000

    displayed first file in db.json on notes

--- Develop/noteTaker.js
+++ Develop/noteTaker.js
@@ -15,31 +15,56 @@ app.use(express.json());
 
 // Routes
 // =============================================================
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-app.get("/api/note", function(req, res) {
+app.get("/api/notes", function(req, res) {
     //read db.json and return all notes
-    fs.readFile(path.join(__dirname, "db", "db.json"));
-  });
-
-  app.get("/api/notes", function(req, res) {
-    // should receive a new note to save to the request body add to the db.json file
-    //then return the new note to the client
-  });
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    
+    fs.readFile(path.join(__dirname, "db", "db.json"), function(err, data) {
+        if (err) throw err; 
+        res.json(JSON.parse(data)) 
+    });
     
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.post("/api/notes", function(req, res) {
+    // // should receive a new note to save to the request body add to the db.json file
+    // //then return the new note to the client
+
+    const newNote = req.body
+    console.log(newNote)
+
+    // fs.appendFile(path.join(__dirname, "db", "db.json"), newNote, function(err) {
+    //     if(err) {
+    //         console.log(err)
+    //     } else {
+    //         console.log(success)
+    //     }
+    // })
+});
+
+app.delete("/api/notes:id", function(req, res) {
+    // should receive a query containing the id of a note to delete
+    // need to read file, find matching id, delete the note, rewrite the file
+
+    // create an object from the db.json file, then use delete to select based on title
+    // either use the new information and append the file
+    // or creat a new object and rewrite the file according to that data
+})
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
   
