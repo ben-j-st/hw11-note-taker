38d20c3dd2ac7742736d4b3fa788802a9b4a96bb
Ben Stephens <ben_j_stephens@hotmail.com>
2020-07-27 11:18:34 +1000

    displaying index and note.html files

--- Develop/noteTaker.js
+++ Develop/noteTaker.js
@@ -2,6 +2,7 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -15,14 +16,26 @@ app.use(express.json());
 // Routes
 // =============================================================
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+app.get("/api/note", function(req, res) {
+    //read db.json and return all notes
+    fs.readFile(path.join(__dirname, "db", "db.json"));
+  });
+
+  app.get("/api/notes", function(req, res) {
+    // should receive a new note to save to the request body add to the db.json file
+    //then return the new note to the client
+  });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+    
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Starts the server to begin listening
 // =============================================================
